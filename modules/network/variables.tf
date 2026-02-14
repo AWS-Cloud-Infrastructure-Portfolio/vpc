0d74ff1585a5602dcb2b14332a54ec27a6926cb7
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 16:58:41 +0100

    feat: add network module variables

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,1 +1,29 @@
+############################################
+# Network Module Variables
+############################################
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for subnets"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags applied to all resources"
+  type        = map(string)
+}
 
