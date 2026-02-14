38f03d40826b7d8d0add76b73eccb077e8da1ab8
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 16:58:18 +0100

    refactor: create network module v2.0

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,1 +1,110 @@
+############################################
+# VPC
+############################################
 
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(var.common_tags, {
+    Name = "lab-vpc"
+  })
+}
+
+############################################
+# Subnets
+############################################
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = merge(var.common_tags, {
+    Name = "lab-public"
+  })
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = merge(var.common_tags, {
+    Name = "lab-private"
+  })
+}
+
+############################################
+# Internet Gateway
+############################################
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.common_tags, {
+    Name = "lab-igw"
+  })
+}
+
+############################################
+# NAT Gateway
+############################################
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = var.common_tags
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  depends_on = [aws_internet_gateway.gw]
+
+  tags = merge(var.common_tags, {
+    Name = "lab-natgw"
+  })
+}
+
+############################################
+# Route Tables
+############################################
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.common_tags, {
+    Name = "lab-public-rt"
+  })
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.common_tags, {
+    Name = "lab-private-rt"
+  })
+}
+
+resource "aws_route" "private_nat_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
