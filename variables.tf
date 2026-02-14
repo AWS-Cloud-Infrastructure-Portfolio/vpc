aa4382bf777f0dc947c6bc8da89e56bd25693a4b
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 13:43:12 +0100

    Enhance variable definitions with descriptions
    Added descriptions for region and admin IP variables.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,26 @@
-variable "region" { type = string }
-variable "vpc_cidr" { type = string }
-variable "public_subnet_cidr" { type = string }
-variable "private_subnet_cidr" { type = string }
-variable "availability_zone" { type = string }
-variable "admin_ip" { type = string }
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "private_subnet_cidr" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+variable "admin_ip" {
+  description = "Admin IP allowed to SSH"
+  type        = string
+}
