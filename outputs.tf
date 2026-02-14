e1fd1b6e8253957c421a82085b8a7cdda9d1e343
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 17:03:49 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.network.vpc_id
 }
 
 output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
-}
-
-output "private_instance_id" {
-  value = aws_instance.app.id
+  value = module.compute.bastion_public_ip
 }
