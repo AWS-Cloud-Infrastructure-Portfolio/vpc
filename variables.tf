b099337f93bd4671966041fbf6b1778b4b1d7651
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 17:03:24 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,9 @@
-variable "region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for VPC"
-}
-
-variable "public_subnet_cidr" {
-  type = string
-}
-
-variable "private_subnet_cidr" {
-  type = string
-}
-
-variable "availability_zone" {
-  type = string
-}
-
-variable "admin_ip" {
-  type        = string
-  description = "Admin IP allowed to SSH (format: x.x.x.x/32)"
-}
-
+variable "region" { type = string }
+variable "vpc_cidr" { type = string }
+variable "public_subnet_cidr" { type = string }
+variable "private_subnet_cidr" { type = string }
+variable "availability_zone" { type = string }
+variable "admin_ip" { type = string }
 variable "instance_type" {
   type    = string
   default = "t3.micro"
