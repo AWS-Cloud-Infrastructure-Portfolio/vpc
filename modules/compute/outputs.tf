89d94cc4426817e06e8fe21473059de6edf9ebe6
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 17:00:02 +0100

    feat: add compute module outputs

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,14 @@
+############################################
+# Compute Module Outputs
+############################################
+
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = aws_instance.bastion.public_ip
+}
+
+output "app_instance_id" {
+  description = "ID of the private application instance"
+  value       = aws_instance.app.id
+}
 
