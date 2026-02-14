e90d2b6866184ebd0f21fc0f691fe3fb31f533d5
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-14 13:39:37 +0100

    Add variables for Terraform configuration

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "region" { type = string }
+variable "vpc_cidr" { type = string }
+variable "public_subnet_cidr" { type = string }
+variable "private_subnet_cidr" { type = string }
+variable "availability_zone" { type = string }
+variable "admin_ip" { type = string }
