4d16d805ca63f15e98da3a9314a3c75369aedd09
SebaSilC <sebastian@playbookvisualarts.com>
2026-03-31 18:52:37 +0200

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,10 +1,6 @@
-############################################
-# Network Module Outputs
-############################################
-
 output "vpc_id" {
   description = "ID of the created VPC"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.this.id
 }
 
 output "public_subnet_id" {
